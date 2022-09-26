ac8464aa873227f7a3d42c3b84b80f1fb491f6e9
abhinav031-tt <113112279+abhinav031-tt@users.noreply.github.com>
2022-09-26 20:47:57 +0530

    Add files via upload
    program run on behalf of using( def )function

---
+++ addition.py
@@ -0,0 +1,17 @@
+''' Abhinav Tiwari
+
+'''
+
+
+
+#Sample List : (8, 2, 3, 0, 7)
+
+
+
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+    return total
+print(sum((8, 2, 3, 0, 7)))
+
